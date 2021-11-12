87c7b554f103b3ab366cc0d303a2ec0cd5f14314
Glitch (hello-express) <none>
2021-11-12 02:43:24 +0000

    🎷🎲 Checkpoint ./public/p5/sketch.js:391930/988

--- public/p5/sketch.js
+++ public/p5/sketch.js
@@ -4,31 +4,37 @@
 	Listens for messages from the server. 
 */
 
-var socket = new WebSocket('wss://' + window.location.host);
+let socket = new WebSocket('wss://' + window.location.host);
+let sensorData; 
+let xPos = 10;
 
 function setup() {
 	// The socket connection needs two event listeners:
 	socket.onopen = openSocket;
 	socket.onmessage = showData;
 
-  console.log(window.location.host);
 	// make a new div and position it at 10, 10:
 	text = createDiv("Sensor reading:");
 	text.position(10,10);
 }
 
+function draw() {
+  text.html(sensorData);
+  text.position(xPos, 10);        // position the text
+}
+
 function openSocket() {
-	text.html("Socket open");
-	socket.send("Hello server");
+	sensorData = "Socket open";
 }
 /*
 showData(), below, will get called whenever there is new data
 from the server. So there's no need for a draw() function:
 */
 function showData(result) {
-	// when the server returns, show the result in the div:
-	text.html("Sensor reading:" + result.data);
-  console.log(result.data);
-	xPos = int(result.data);        // convert result to an integer
-	text.position(xPos, 10);        // position the text
+	// when you 
+  sensorData = result.data;
+  let incoming = JSON.parse(sensorData);
+	xPos = int(incoming.sensor);        // convert result to an integer
+  
+
 }
