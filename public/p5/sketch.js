e477552f119b986b3fdb5432568a5481c282d444
Glitch (hello-express) <none>
2021-11-12 02:55:25 +0000

    🏪🍁 Checkpoint ./public/p5/sketch.js:391930/2583

--- public/p5/sketch.js
+++ public/p5/sketch.js
@@ -2,6 +2,13 @@
 	p5.js websocket client
 	makes a websocket client connection to a websocket server.
 	Listens for messages from the server. 
+  
+  This won't work on editor.p5.js unless you modify
+  the websocket server address. 
+  
+  created 12 Apr 2021
+  modified 11 Nov 2021
+  by Tom Igoe
 */
 
 let socket = new WebSocket('wss://' + window.location.host);
@@ -11,7 +18,7 @@ let xPos = 10;
 function setup() {
 	// The socket connection needs two event listeners:
 	socket.onopen = openSocket;
-	socket.onmessage = showData;
+	socket.onmessage = getData;
 
 	// make a new div and position it at 10, 10:
 	text = createDiv("Sensor reading:");
@@ -27,14 +34,14 @@ function openSocket() {
 	sensorData = "Socket open";
 }
 /*
-showData(), below, will get called whenever there is new data
-from the server. So there's no need for a draw() function:
+getData(), below, will get called whenever there is new data
+from the server:
 */
-function showData(result) {
-	// when you 
+function getData(result) {
+	// when you get some data, put it in sensorData:
   sensorData = result.data;
+  // parse it to see if it's valid JSON:
   let incoming = JSON.parse(sensorData);
-	xPos = int(incoming.sensor);        // convert result to an integer
-  
-
+  // if it is, get the sensor value for the X position
+	xPos = int(incoming.sensor);
 }
