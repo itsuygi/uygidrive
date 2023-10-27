7360a46109faf5caab7b4541c008a24b7c913935
Glitch (hello-express) <none>
2023-10-27 21:11:39 +0000

    🌑🗼 Checkpoint ./public/upload.css:23033352/3229 ./public/upload.html:23033352/2587 ./public/upload.js:23033352/2613

--- public/upload.js
+++ public/upload.js
@@ -1,24 +1,46 @@
 function setup() {
-  document.getElementById('uploadForm').addEventListener('submit', function (e) {
+  const uploadForm = document.getElementById('uploadForm');
+  const fileInput = document.getElementById('fileInput');
+  const progressBar = document.getElementById('progressBar');
+  const status = document.getElementById('status');
+  const successBox = document.getElementById('successBox');
+  const fileURL = document.getElementById('fileURL');
+  const copyURLButton = document.getElementById('copyURLButton');
+
+  uploadForm.addEventListener('submit', function (e) {
     e.preventDefault();
     const formData = new FormData(this);
     const xhr = new XMLHttpRequest();
-    
+
     xhr.open('POST', '/upload', true);
 
     xhr.upload.onprogress = function (e) {
       if (e.lengthComputable) {
         const percentComplete = Math.floor((e.loaded / e.total) * 100);
-        document.getElementById('status').textContent = 'Uploading: ' + percentComplete + '%';
+        progressBar.style.width = percentComplete + '%';
+        status.textContent = 'Uploading: ' + percentComplete + '%';
       }
     };
 
     xhr.onload = function () {
       if (xhr.status === 200) {
-        var url = xhr.responseText
-        document.getElementById('status').innerHTML = 'File uploaded. URL: <a href="' + url + '"> ' + url + "</a>";
+        var url = xhr.responseText;
+        status.style.display = 'none';
+        successBox.style.display = 'block';
+        fileURL.textContent = url;
+        fileURL.href = url;
+
+        copyURLButton.addEventListener('click', function () {
+          const textArea = document.createElement('textarea');
+          textArea.value = url;
+          document.body.appendChild(textArea);
+          textArea.select();
+          document.execCommand('copy');
+          document.body.removeChild(textArea);
+          copyURLButton.textContent = 'URL Copied!';
+        });
       } else {
-        document.getElementById('status').innerHTML = 'Error while uploading!';
+        status.textContent = 'Error while uploading!';
       }
     };
 
