9156672c9a2616e73fdc5638f65697a8e9675eac
Glitch (rolive) <none>
2023-10-29 21:48:52 +0000

    🏃🕰 Checkpoint ./public/stream/stream.js:23033352/2801

--- public/stream/stream.js
+++ public/stream/stream.js
@@ -30,32 +30,16 @@ function setup() {
     sendMessage("POST", "/sendMessageToTopic", {"topic": topic, "message": {"type": "play", "message": outgoingText.value}})
   });
   
-  sendMessage("GET", "/getStreamId").then((obtainedStreamId) => {
-    console.log("Obtained stream id: ", obtainedStreamId)
-    streamId.innerText = obtainedStreamId
+  
+  sendMessage("GET", "/getStreamId").then((value) => {
+    console.log("Obtained stream id: ", value)
+    streamId.innerText = value
 
-    topic = obtainedStreamId
+    topic = value
   });
 }
 
-function openSocket(url) {
-  // open the socket:
-  connectionSpan.innerHTML = "Connecting..."
-  socket = new WebSocket(url);
-  socket.addEventListener('open', openConnection);
-  socket.addEventListener('close', closeConnection);
-  socket.addEventListener('message', readIncomingMessage);
-}
 
-
-function changeConnection(event) {
-  // open the connection if it's closed, or close it if open:
-  if (socket.readyState === WebSocket.CLOSED) {
-    openSocket(serverURL);
-  } else {
-    socket.close();
-  }
-}
 
 function openConnection() {
   // display the change of state:
@@ -70,69 +54,27 @@ function closeConnection() {
 }
 
 
-function readIncomingMessage(event) {
-  // display the incoming message:
-  incomingSpan.innerHTML = event.data;
-  
-  let dataJson = JSON.parse(event.data);
-  console.log(dataJson);
-  
-  if (dataJson.type == "connection" && dataJson.message == "successfull") {
-    connectionStatus.innerHTML = "Listening to stream"
-    connectionStatus.style.color = "green"
-    connectWidget.style.display = "none"
-    
-  } else if (dataJson.type == "play") {
-    audio.src = dataJson.message
-    audio.load();
-  } else if (dataJson.type == "stop") {
-    audio.src = ""
-  } else if (dataJson.type == "resumePlay") {
-    connectionStatus.innerHTML = "Connected to stream and synced"
-    connectionStatus.style.color = "green"
-    connectWidget.style.display = "none"
-    
-    var timestamp = new Date(dataJson.message.timestamp)
-    var currentTime = Date.now()
-    var timeBetween = Math.abs(currentTime - timestamp) / 1000;
-    
-    console.log(timestamp);
-    console.log(currentTime);
-    console.log(timeBetween);
-    
-    audio.src = dataJson.message.url
-    audio.load();
-    audio.currentTime = timeBetween
-  }
-}
+
 
 async function sendMessage(method, url, message) {
   //if the socket's open, send a message:
-  const xhr = new XMLHttpRequest();
-
-  xhr.open(method, url, true);
-
-  xhr.onload = await function () {
-     if (xhr.status === 200) {
-       var result = xhr.responseText;
-       
-       console.log(result)
-     
-       return result;
-    } else {
-       return 'Error!';
-    }
-  };
-
-  await xhr.send(message);
+  let myPromise = new Promise(function(resolve) {
+    let req = new XMLHttpRequest();
+    req.
+    req.open(method, url);
+    req.onload = function() {
+      if (req.status == 200) {
+        resolve(req.response);
+      } else {
+        resolve("error");
+      }
+    };
+    req.send(JSON.stringify(message));
+  });
+  
+  return await myPromise;
 }
 
-
-function ping() {
-  sendMessage("keepAlive", "ping")
-}
-
-setInterval(ping, 30000);
 
 
 window.addEventListener('load', setup);
