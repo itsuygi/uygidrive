04c8863696de8adc80838b6919b71b9b764fe597
Glitch (rolive) <none>
2023-11-01 17:52:25 +0000

    🏇💎 Checkpoint ./public/common/color-mode.js:23033352/24364 ./public/upload/upload.html:23033352/178 ./public/common/style-dark.css:23033352/118693 ./public/stream/stream.html:23033352/92 ./public/index.html:23033352/92

--- public/common/color-mode.js
+++ public/common/color-mode.js
@@ -1,17 +1,17 @@
+function main() {
+  const themeToggle = document.getElementById("theme-toggle");
+  const themeIcon = document.getElementById("theme-icon");
+  const darkThemeLink = document.getElementById("dark-theme");
 
+  themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
 
-const themeToggle = document.getElementById("theme-toggle");
-const themeIcon = document.getElementById("theme-icon");
-const darkThemeLink = document.getElementById("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+      themeIcon.innerHTML = '<i class="fas fa-sun"></i>'; // Güneş sembolü
+    } else {
+      themeIcon.innerHTML = '<i class="fas fa-moon"></i>'; // Ay sembolü
+    }
+  });
+}
 
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
-
-  if (document.body.classList.contains("dark-theme")) {
-    themeIcon.innerHTML = '<i class="fas fa-sun"></i>'; // Güneş sembolü
-  } else {
-    themeIcon.innerHTML = '<i class="fas fa-moon"></i>'; // Ay sembolü
-  }
-});
-
-window.addEventListener("load", setup);
+window.addEventListener("load", main);
