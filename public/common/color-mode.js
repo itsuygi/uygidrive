c5e6a97a457c7a60e9c8f0862cbfede948d0dd8c
Glitch (rolive) <none>
2023-11-01 19:15:50 +0000

    ⏲👻 Checkpoint ./public/common/color-mode.js:23033352/1634

--- public/common/color-mode.js
+++ public/common/color-mode.js
@@ -1,17 +1,30 @@
-function main() {
+window.addEventListener("load", function(){
   const themeToggle = document.getElementById("theme-toggle");
   const themeIcon = document.getElementById("theme-icon");
-  const darkThemeLink = document.getElementById("dark-theme");
+
+
+  const isDarkMode = localStorage.getItem("darkMode") === "true";
+
+
+  if (isDarkMode) {
+    document.body.classList.add("dark-theme");
+    themeIcon.innerHTML = '<i class="fas fa-sun"></i>'; 
+  } else {
+    document.body.classList.remove("dark-theme");
+    themeIcon.innerHTML = '<i class="fas fa-moon"></i>'; 
+  }
 
   themeToggle.addEventListener("click", () => {
+    // Koyu mod sınıfını ekleyip kaldır
     document.body.classList.toggle("dark-theme");
 
+    // Tema değiştikçe localStorage'a kaydet
     if (document.body.classList.contains("dark-theme")) {
       themeIcon.innerHTML = '<i class="fas fa-sun"></i>'; // Güneş sembolü
+      localStorage.setItem("darkMode", "true");
     } else {
       themeIcon.innerHTML = '<i class="fas fa-moon"></i>'; // Ay sembolü
+      localStorage.setItem("darkMode", "false");
     }
   });
-}
-
-window.addEventListener("load", main);
+});
--- public/script.js
+++ public/script.js
@@ -39,20 +39,6 @@ function setup() {
     handleVolume();
   };
   openSocket(serverURL);
-  
-  const themeToggle = document.getElementById("theme-toggle");
-  const themeIcon = document.getElementById("theme-icon");
-  const darkThemeLink = document.getElementById("dark-theme");
-
-  themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-theme");
-
-    if (document.body.classList.contains("dark-theme")) {
-      themeIcon.innerHTML = '<i class="fas fa-sun"></i>'; // Güneş sembolü
-    } else {
-      themeIcon.innerHTML = '<i class="fas fa-moon"></i>'; // Ay sembolü
-    }
-  });
 }
 
 function openSocket(url) {
