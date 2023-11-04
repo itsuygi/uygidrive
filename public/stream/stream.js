f85bd7b58a531ed70b1a32507e456e60ee266140
Glitch (rolive) <none>
2023-11-04 13:30:29 +0000

    ☎️🌏 Checkpoint ./public/stream/stream.js:23033352/1629

--- public/stream/stream.js
+++ public/stream/stream.js
@@ -69,27 +69,42 @@ function readIncomingMessage(event) {
 
 function sendSocketMessage(type, message) {
   let sendJson = { type: type, message: message };
+  
+  waitForSocketConnection(socket, function(){
+      console.log("Waited for connection.");
+      socket.send(JSON.stringify(sendJson));
+  });
+}
 
-  if (socket.readyState === WebSocket.OPEN) {
-    socket.send(JSON.stringify(sendJson));
-  } else {
-    console.log("[Socket]: tried to sent socket message but there is no socket connection")
-  }
+function waitForSocketConnection(socket, callback){
+    setTimeout(
+        function () {
+            if (socket.readyState === 1) {
+                console.log("Connection is made")
+                if (callback != null){
+                    callback();
+                }
+            } else {
+                console.log("Waiting for connection...")
+                waitForSocketConnection(socket, callback);
+            }
+
+        }, 5); // wait 5 milisecond for the connection
 }
 
 function handleAudio(url, state){
-  if (state == true) {
+  /*if (state == true) {
     audio.src = url
     audio.load();
   } else {
      audio.src = ""
-  }
+  }*/
   
 }
 
 async function sendMessage(method, url, message) {
   //if the socket's open, send a message:
-  let myPromise = new Promise(function(resolve) {
+  let messagePromise = new Promise(function(resolve) {
     let req = new XMLHttpRequest();
     req.open(method, url);
     req.setRequestHeader("Content-Type", "application/json");
@@ -103,10 +118,13 @@ async function sendMessage(method, url, message) {
     req.send(JSON.stringify(message));
   });
   
-  return await myPromise;
+  return await messagePromise;
 }
 
-
+function ping() {
+  sendSocketMessage("keepAlive", "ping");
+}
+setInterval(ping, 30000);
 
 window.addEventListener('load', setup);
 
