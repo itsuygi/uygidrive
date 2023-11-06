cd7028a4a442499a3ed7a0bef6eb2defa2f65b36
Glitch (rolive) <none>
2023-11-06 18:11:56 +0000

    🚛🚨 Checkpoint ./server.js:23033352/195

--- server.js
+++ server.js
@@ -208,6 +208,10 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
+function generateAccessToken(id) {
+  return jwt.sign({'id': id}, process.env.TOKEN_SECRET, { expiresIn: '10h' });
+}
+
 
 // Uploading
 
