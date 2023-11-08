54e6f0fa205d5f0c58fcb9246bd496485ece989e
Glitch (rolive) <none>
2023-11-08 16:22:57 +0000

    🌌🏨 Checkpoint ./public/api/api.js:23033352/1125

--- public/api/api.js
+++ public/api/api.js
@@ -30,25 +30,31 @@ document.addEventListener("DOMContentLoaded", function () {
         accordionTemplate.innerHTML = `
           <div class="card-header" id="endpoint${i}">
             <h2 class="mb-0">
-              <button class="btn btn-link endpoint-title" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
+              <button class="btn btn-link endpoint-title" onclick="$('#collapse${i}').collapse('toggle');" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
                 ${apiData[i].endpoint} - GET
               </button>
             </h2>
           </div>
 
-          <div id="collapse${i}" class="collapse show" aria-labelledby="endpoint${i}" data-parent="#apiEndpoints">
+          <div id="collapse${i}" class="collapse" aria-labelledby="endpoint${i}" data-parent="#apiEndpoints">
+            <div class="card-title">
+             test
+            </div>
             <div class="card-body">
               <p class="endpoint-description">${apiData[i].description}</p>
+            </div>
+            <div class="card-body">
               <div class="request-body">${apiData[i].body}</div>
-              <div class="response">${apiData[i].response}</div>
+            </div>
+            <div class="card-body">
+              <div class="response">${apiData[i].response}</div> 
             </div>
           </div>
         `;
 
       accordionContainer.appendChild(accordionTemplate);
     }
-  
-    const accordion = new bootstrap.Collapse(document.querySelectorAll('.accordion-button'));
+
 });
 
 
