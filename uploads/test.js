00b8d08b3902a8858a12cd976ca482ea598d7088
Glitch (songroom) <none>
2023-11-12 20:10:33 +0000

    🐷😀 Checkpoint ./uploads/test.js:23033352/5097

--- uploads/test.js
+++ uploads/test.js
@@ -49,44 +49,42 @@
 
 window.addEventListener('load', function() {
   document.getElementById('start').addEventListener('click', function(){
-    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const audioElement = document.getElementById('audio');
+    // Web Audio API'yi başlat
+  var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+  // Ses dosyanızı yükleyin
+  const audioElement = document.getElementById('audio');
     
-    audioElement.crossOrigin = "anonymous";
-    
-    audioElement.src = "https://cdn.glitch.global/09cea16e-eb78-4d1a-81a4-6b24ad8afe1c/break-my-heart.mp3?v=1699817356279"
-    
-    audioElement.play();
-    var audioSource = audioContext.createMediaElementSource(audioElement);
-    var panner = audioContext.createPanner();
+  audioElement.crossOrigin = "anonymous";
+  audioElement.src = "https://cdn.glitch.global/09cea16e-eb78-4d1a-81a4-6b24ad8afe1c/break-my-heart.mp3?v=1699817356279"
+  var audioSource = audioContext.createMediaElementSource(audioElement);
 
-    audioSource.connect(panner);
-    panner.connect(audioContext.destination);
+  // Ses düğümünü oluşturun
+  var panner = audioContext.createPanner();
+  panner.setPosition(0, 0, 10); // Sesin konumunu belirleyin (örnekte x: 1, y: 0, z: 0)
 
-    audioElement.play();
+  // Ses düğümünü bağlayın
+  audioSource.connect(panner);
+  panner.connect(audioContext.destination);
 
-    // Başlangıçta kullanılacak olan açı
-    var angle = 0;
+  // Ses dosyasını çalın
+  audioElement.play();
 
-    // Ses konumunu güncelleyen fonksiyon
-function updateSoundPosition() {
-  // Başınızın etrafında linear bir dönüş yapın
-  var x = Math.cos(angle);
-  var y = Math.sin(angle);
-  var z = 30;  // Z ekseni için değeri istediğiniz gibi ayarlayabilirsiniz
+  // Ses konumunu güncelleyin
+  function updateSoundPosition(x, y, z) {
+    panner.setPosition(x, y, z);
+  }
 
-  // Ses konumunu güncelle
-  panner.setPosition(x, y, z);
+  // Örnek olarak, ses konumunu her 5 saniyede bir güncelleyebilirsiniz
+  setInterval(function() {
+    // Yeni konum değerlerini belirleyin
+    var newX = Math.random() * 2 - 1; // -1 ile 1 arasında rastgele bir x değeri
+    var newY = Math.random() * 2 - 2; // -1 ile 1 arasında rastgele bir y değeri
+    var newZ = 5; // -1 ile 1 arasında rastgele bir z değeri
 
-  // Açıyı arttır (dönüş hızını ve yöndeğiştirmeyi ayarlamak için)
-  angle += 0.09;  // Bu değeri istediğiniz gibi ayarlayabilirsiniz
+    // Ses konumunu güncelleyin
+    updateSoundPosition(newX, newY, newZ);
+  }, 2000); // Her 5 saniyede bir güncelle
 
-  // Yeniden çağır
-  setTimeout(updateSoundPosition, 10);
-}
-
-// İlk çağrıyı yap
-updateSoundPosition();
-  })
 });
-
+})
