f44114c87e17d122086110adc82ff7390ddee039
Glitch (songroom) <none>
2023-11-12 11:55:36 +0000

    🚂👫 Checkpoint ./uploads/test.js:23033352/1232

---
+++ uploads/test.js
@@ -0,0 +1,33 @@
+// Web Audio API'yi başlat
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+// Ses dosyanızı yükleyin
+var audioElement = new Audio('https://songroom.glitch.me/music/cool-music.mp3');
+var audioSource = audioContext.createMediaElementSource(audioElement);
+
+// Ses düğümünü oluşturun
+var panner = audioContext.createPanner();
+panner.setPosition(1, 0, 0); // Sesin konumunu belirleyin (örnekte x: 1, y: 0, z: 0)
+
+// Ses düğümünü bağlayın
+audioSource.connect(panner);
+panner.connect(audioContext.destination);
+
+// Ses dosyasını çalın
+audioElement.play();
+
+// Ses konumunu güncelleyin
+function updateSoundPosition(x, y, z) {
+  panner.setPosition(x, y, z);
+}
+
+// Örnek olarak, ses konumunu her 5 saniyede bir güncelleyebilirsiniz
+setInterval(function() {
+  // Yeni konum değerlerini belirleyin
+  var newX = Math.random() * 2 - 1; // -1 ile 1 arasında rastgele bir x değeri
+  var newY = Math.random() * 2 - 1; // -1 ile 1 arasında rastgele bir y değeri
+  var newZ = Math.random() * 2 - 1; // -1 ile 1 arasında rastgele bir z değeri
+
+  // Ses konumunu güncelleyin
+  updateSoundPosition(newX, newY, newZ);
+}, 5000); // Her 5 saniyede bir güncelle
