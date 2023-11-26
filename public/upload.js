103da27d9c9587a724e13d52dd7e61b01223f2af
Glitch (songroom) <none>
2023-11-26 13:11:03 +0000

    🍼👗 Checkpoint ./public/upload.js:23033352/935

--- public/upload.js
+++ public/upload.js
@@ -21,6 +21,19 @@ function setup() {
   const searchBox = document.getElementById('searchBox');
   const audio = document.getElementById('audio');
   const sortSelect = document.getElementById('sortSelect');
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyCILjXwpyNilznxbFTWC9J2Ys2JdJTX0vg",
+    authDomain: "uygidrive.firebaseapp.com",
+    projectId: "uygidrive",
+    storageBucket: "uygidrive.appspot.com",
+    messagingSenderId: "210147915736",
+    appId: "1:210147915736:web:9e3548e815091d7512a4ad",
+    measurementId: "G-GB12PYXWF7"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
   uploadForm.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -178,18 +191,6 @@ function setup() {
      }
    });
   
-    const firebaseConfig = {
-      apiKey: "AIzaSyCILjXwpyNilznxbFTWC9J2Ys2JdJTX0vg",
-      authDomain: "uygidrive.firebaseapp.com",
-      projectId: "uygidrive",
-      storageBucket: "uygidrive.appspot.com",
-      messagingSenderId: "210147915736",
-      appId: "1:210147915736:web:9e3548e815091d7512a4ad",
-      measurementId: "G-GB12PYXWF7"
-    };
-
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
   
 }
 
