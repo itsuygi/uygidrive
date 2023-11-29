5f2c28bca40a2ff60868cc0a37364f8b945c479f
Glitch (songroom) <none>
2023-11-29 14:57:05 +0000

    🗃👜 Checkpoint ./public/upload.ejs:23033352/1071 ./public/upload.css:23033352/1203 ./public/common/notify.js:23033352/824

--- public/common/notify.js
+++ public/common/notify.js
@@ -623,3 +623,24 @@
 	});
 
 }));
+
+
+var modal = document.getElementById('previewPopup');
+
+var span = document.getElementsByClassName("close")[0];
+
+function  {
+    modal.style.display = "block";
+}
+
+// Kullanıcı <span> (x) düğmesini tıkladığında, popup
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Kullanıcı modelden başka herhangi bir yeri tıklattıysa, onu kapatın.
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
