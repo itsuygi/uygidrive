fcedead163d8f351c6599a6a73ccdcee222f9be8
Glitch (songroom) <none>
2023-12-05 18:34:52 +0000

    🚠🐯 Checkpoint ./server.js:23033352/31

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.get("/searchYT", async (req, res) => {
     console.log(searchResults.videos)
     
     searchResults.videos.forEach((video) => {
-      result.push({title: video.title, url: video.url, id: video.videoId})
+      result.push({title: video.title, url: hostUrl + "/downloadYT?url=" + video.url, id: video.videoId})
     });
      
     console.log(result)
