e5cbbd1a2bffef64881adeada9dde485e7dd003a
Glitch (songroom) <none>
2024-01-15 18:48:51 +0000

    👙🌖 Checkpoint ./public/common/upload.js:23033352/3889 ./public/common/v2design.html:23033352/11796

--- public/common/upload.js
+++ public/common/upload.js
@@ -77,6 +77,8 @@ window.onload = function() {
     
   }
   
+  loadList()
+  
   function loadList() {
     fileList.innerHTML = 'Loading...';
     
@@ -115,75 +117,19 @@ window.onload = function() {
           if (file.url) {
             url = file.url
           }
-          const fileItem = document.createElement('div');
-          fileItem.classList.add('file-item');
           
-          const fileName = document.createElement('a');
           
-          fileName.textContent = file.name
-          fileName.classList.add("file-title")
-          fileName.href = "javascript:void(0);"
-          
-          fileName.addEventListener('click', function () {
-            openModal(url)
-          });
-          
-          fileItem.appendChild(fileName);
-          
-          const fileSize = document.createElement('small');
-          fileSize.textContent = file.size
-          fileSize.classList.add('file-size');
-          
-          fileItem.appendChild(fileSize);
-
-          const downloadButton = document.createElement('button');
-          downloadButton.textContent = 'Download';
-          downloadButton.classList.add('copy-link-button');
-          downloadButton.addEventListener('click', function () {
-            document.getElementById('download_iframe').src = url; 
-          });
-          fileItem.appendChild(downloadButton);
-          
-          const deleteButton = document.createElement('button');
-          deleteButton.innerHTML = `
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M4 7l16 0" />
-            <path d="M10 11l0 6" />
-            <path d="M14 11l0 6" />
-            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-          </svg>
-          `
-          deleteButton.classList.add('delete-button');
-          deleteButton.addEventListener('click', function () {
-            deleteFile(file.name);
-          });
-          fileItem.appendChild(deleteButton);
-          
-          const shareButton = document.createElement('button');
-          shareButton.innerHTML = `
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-share" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M6 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-            <path d="M18 6m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-            <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-            <path d="M8.7 10.7l6.6 -3.4" />
-            <path d="M8.7 13.3l6.6 3.4" />
-          </svg>
-          `
-          shareButton.classList.add('share-button');
-          shareButton.addEventListener('click', function () {
-            shareFile(file.name)
-          });
-          fileItem.appendChild(shareButton);
-          
-          const line = document.createElement('div');
-          line.classList.add('line');
-          
-          fileList.appendChild(fileItem);
-          
-          fileList.appendChild(line);
+          let fileHTML = `
+          <div class="file-card">
+            <div class="file-actions">
+              <i class="fas fa-ellipsis-v" aria-hidden="true"></i>
+            </div>
+            <h5>${file.name}</h5>
+            <p>${file.size}</p>
+            
+          </div>
+        `
+          fileList.insertAdjacentHTML( 'beforeend', fileHTML );
         });
         
         if (firstLoad == false) {
@@ -199,13 +145,13 @@ window.onload = function() {
     listXhr.send()
   };
   
-  sortSelect.addEventListener('change', function () {
+  /*sortSelect.addEventListener('change', function () {
      const selectedValue = sortSelect.value;
      
      sort = selectedValue
      currentPage = 1
      loadList()
-   });
+   });*/
   
    nextPage.addEventListener('click', function () {
      currentPage++
