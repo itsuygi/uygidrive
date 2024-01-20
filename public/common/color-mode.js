ec7943e2cdf0438405d5fe7096581c22efdcc2bc
Glitch (songroom) <none>
2024-01-20 19:29:34 +0000

    🛩⛹ Checkpoint ./public/uploadv2.ejs:23033352/350 ./public/uploadv2.css:23033352/39 ./public/common/color-mode.js:23033352/5379

--- public/common/color-mode.js
+++ public/common/color-mode.js
@@ -1,3 +1,17 @@
+function changeColorMode() {
+  if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+    document.documentElement.setAttribute('data-bs-theme','light')
+    document.getElementById("theme-icon").innerHTML = '<i class="fas fa-moon"></i>'; 
+    
+    localStorage.setItem("darkMode", "false");
+  }
+  else {
+    document.documentElement.setAttribute('data-bs-theme','dark')
+    document.getElementById("theme-icon").innerHTML = '<i class="fas fa-sun"></i>'; 
+    localStorage.setItem("darkMode", "true");
+  }
+}
+
 window.addEventListener("load", function(){
   const themeToggle = document.getElementById("theme-toggle");
   const themeIcon = document.getElementById("theme-icon");
@@ -6,24 +20,19 @@ window.addEventListener("load", function(){
 
 
   if (isDarkMode) {
-    document.body.classList.add("dark-theme");
+    document.documentElement.setAttribute('data-bs-theme','dark')
     themeIcon.innerHTML = '<i class="fas fa-sun"></i>'; 
   } else {
-    document.body.classList.remove("dark-theme");
+    document.documentElement.setAttribute('data-bs-theme','light')
     themeIcon.innerHTML = '<i class="fas fa-moon"></i>'; 
   }
+  
+  document.getElementById('btnSwitch').addEventListener('click',()=>{
+      
+  })
 
   themeToggle.addEventListener("click", () => {
-    // Koyu mod sınıfını ekleyip kaldır
-    document.body.classList.toggle("dark-theme");
-
-    // Tema değiştikçe localStorage'a kaydet
-    if (document.body.classList.contains("dark-theme")) {
-      themeIcon.innerHTML = '<i class="fas fa-sun"></i>'; // Güneş sembolü
-      localStorage.setItem("darkMode", "true");
-    } else {
-      themeIcon.innerHTML = '<i class="fas fa-moon"></i>'; // Ay sembolü
-      localStorage.setItem("darkMode", "false");
-    }
+    changeColorMode()
+    
   });
 });
